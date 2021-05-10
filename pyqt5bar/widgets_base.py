7cf8002f9ec3bad0dc5cfd6ca4c75969e80191b8
Yusuf Mohammad <yousufinternet@gmail.com>
2021-05-11 01:54:31 +0300

    New Bases

--- pyqt5bar/widgets_base.py
+++ pyqt5bar/widgets_base.py
@@ -8,6 +8,7 @@ from PyQt5 import QtCore, QtWidgets, Qt
 
 default_style = {'background': 'transparent', 'padding': '0px', 'margin': '0px'}
 
+
 class LabelWithSignals(QtWidgets.QLabel):
     '''
     QLabel with signals for hover, click and double click event
@@ -15,11 +16,22 @@ class LabelWithSignals(QtWidgets.QLabel):
     clicked = QtCore.pyqtSignal()
     hovered = QtCore.pyqtSignal()
     doubleClicked = QtCore.pyqtSignal()
+    scrollUp = QtCore.pyqtSignal()
+    scrollDown = QtCore.pyqtSignal()
+
+    def wheelEvent(self, ev):
+        print(ev.angleDelta().y())
+        if ev.angleDelta().y() < 0:
+            print('Down')
+            self.scrollDown.emit()
+        else:
+            print('Up')
+            self.scrollUp.emit()
 
     def mouseReleaseEvent(self, ev):
         self.clicked.emit()
 
-    def focusInEvent(self, ev):
+    def enterEvent(self, ev):
         self.hovered.emit()
 
     def mouseDoubleClickEvent(self, ev):
@@ -35,15 +47,17 @@ class GroupWidget(QtWidgets.QFrame):
             self.hlayout.addWidget(wdgt)
         self.setLayout(self.hlayout)
 
-        self.props = default_style
+        self.props = default_style.copy()
         for k, v in kwargs.items():
             self.props[k] = v
         self.stylize()
 
     def stylize(self):
-        stylesheet = " ".join(f'{k.replace("_", "-")}: {v};'
-                              for k, v in self.props.items())
+        stylesheet = " ".join(
+            f'{k.replace("_", "-")}: {v};'
+            for k, v in self.props.items())
         self.setStyleSheet(stylesheet)
+        # self.setProperty('group_frame', True)
 
 
 class BaseWidget(QtWidgets.QWidget):
@@ -52,7 +66,7 @@ class BaseWidget(QtWidgets.QWidget):
     '''
     def __init__(self, **kwargs):
         super().__init__()
-        self.props = default_style
+        self.props = default_style.copy()
         for k, v in kwargs.items():
             self.props[k] = v
         self.stylize()
@@ -68,19 +82,32 @@ class BaseWidget(QtWidgets.QWidget):
 
 class TextWidget(BaseWidget):
     def __init__(self, text, click_func=None, hover_func=None,
-                 doubleclick_func=None, **kwargs):
+                 doubleclick_func=None, scrollup_func=None,
+                 scrolldown_func=None, **kwargs):
         super().__init__(**kwargs)
+        self.click_func = click_func
+        self.hover_func = hover_func
+        self.doubleclick_func = doubleclick_func
+        self.scrollup_func = scrollup_func
+        self.scrolldown_func = scrolldown_func
         self.hbox = QtWidgets.QHBoxLayout()
         self.hbox.setContentsMargins(0, 0, 0, 0)
         self.label = LabelWithSignals(text)
-        if click_func is not None:
-            self.label.clicked.connect(click_func)
-        if hover_func is not None:
-            self.label.hovered.connect(hover_func)
-        if doubleclick_func is not None:
-            self.label.doubleClicked.connect(doubleclick_func)
         self.setLayout(self.hbox)
         self.hbox.addWidget(self.label)
+        self.connect_signals()
+
+    def connect_signals(self):
+        if self.click_func is not None:
+            self.label.clicked.connect(self.click_func)
+        if self.hover_func is not None:
+            self.label.hovered.connect(self.hover_func)
+        if self.doubleclick_func is not None:
+            self.label.doubleClicked.connect(self.doubleclick_func)
+        if self.scrollup_func is not None:
+            self.label.scrollUp.connect(self.scrollup_func)
+        if self.scrolldown_func is not None:
+            self.label.scrollDown.connect(self.scrolldown_func)
 
 
 class SubProcessObject(QtCore.QObject):
@@ -107,25 +134,32 @@ class SubProcessObject(QtCore.QObject):
     def start_updater_proc(self):
         if self.update_proc is None:
             return
-        self.update_p = subprocess.Popen(self.update_proc, text=True, shell=True, stdout=subprocess.PIPE)
+        self.update_p = subprocess.Popen(
+            self.update_proc, text=True, shell=True,
+            stdout=subprocess.PIPE)
     
     def wait_updater(self):
+        '''
+        return early if updater produced something
+        '''
         ready, _, _ = select.select(
             [self.update_p.stdout], [], [], self.update_period)
-        print(ready)
         if ready:
-            print(ready[0].readline())
+            _ = ready[0].readline()
             return True
-        else:
-            return False
+        return False
 
     def start_process(self):
         while True:
             if self.cmd is not None:
-                P = subprocess.Popen(
-                    self.cmd, text=True, shell=True, stdout=subprocess.PIPE)
-                P.wait()
-                out = P.stdout.read()
+                try:
+                    P = subprocess.Popen(
+                        self.cmd, text=True, shell=True,
+                        stdout=subprocess.PIPE)
+                    P.wait()
+                    out = P.stdout.read()
+                except subprocess.CalledProcessError:
+                    out = ''
             if self.func is not None:
                 out = self.func()
             if self.post_proc_func is not None:
@@ -171,5 +205,29 @@ class SelfUpdatingWidget(TextWidget):
         self.thread.start()
 
     def update_widget(self, text):
-        self.current_text = text
+        if self.label.text() == text:
+            return
         self.label.setText(text)
+
+
+class SelfUpdatingWidgets(GroupWidget):
+    def __init__(
+            self, wdgts, cmd=None, func=None, update_period=0,
+            update_proc=None, post_proc_func=None, **kwargs):
+        super().__init__(wdgts, **kwargs)
+
+        self.func_obj = SubProcessObject(
+            cmd, func, update_period, update_proc, post_proc_func)
+
+        self.thread = QtCore.QThread()
+        self.func_obj.moveToThread(self.thread)
+        self.func_obj.update_signal.connect(self.update_widget)
+        self.thread.started.connect(self.func_obj.start_process)
+        self.thread.start()
+        self.widgets = wdgts
+
+    def update_widgets(self, output:list):
+        '''
+        Override with your logic
+        '''
+        pass
